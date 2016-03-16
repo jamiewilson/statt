7a9d1bed3bfb3b2e24434e2377adbb6b0726bcc9
Jamie Wilson <jamie@jamiewilson.io>
2016-03-16 17:39:35 -0500

    double quotes

--- config/metalsmith.js
+++ config/metalsmith.js
@@ -1,38 +1,38 @@
-var metalsmith   = require('metalsmith');
-var assets       = require('metalsmith-assets');
-var collections  = require('metalsmith-collections');
-var copy         = require('metalsmith-static');
-var datamarkdown = require('metalsmith-data-markdown');
-var dateFormat   = require('metalsmith-date-formatter');
-var drafts       = require('metalsmith-drafts');
-var inplace      = require('metalsmith-in-place');
-var layouts      = require('metalsmith-layouts');
-var markdown     = require('metalsmith-markdown');
-var permalinks   = require('metalsmith-permalinks');
-var prefix       = require('metalsmith-autoprefixer');
-var sass         = require('metalsmith-sass');
-var uglify       = require('metalsmith-uglify');
-var watch        = require('metalsmith-watch');
+var metalsmith   = require("metalsmith");
+var assets       = require("metalsmith-assets");
+var collections  = require("metalsmith-collections");
+var copy         = require("metalsmith-static");
+var datamarkdown = require("metalsmith-data-markdown");
+var dateFormat   = require("metalsmith-date-formatter");
+var drafts       = require("metalsmith-drafts");
+var inplace      = require("metalsmith-in-place");
+var layouts      = require("metalsmith-layouts");
+var markdown     = require("metalsmith-markdown");
+var permalinks   = require("metalsmith-permalinks");
+var prefix       = require("metalsmith-autoprefixer");
+var sass         = require("metalsmith-sass");
+var uglify       = require("metalsmith-uglify");
+var watch        = require("metalsmith-watch");
 
 //========================
 //  Log and config files
 //========================
 
-var log = require('./log');
+var log = require("./log");
 
 var config = {
-  assets:      require('./assets'),
-  collections: require('./collections'),
-  copy:        require('./copy'),
-  dateFormat:  require('./dateFormat'),
-  defaults:    require('./defaults'),
-  helpers:     require('./helpers'),
-  layouts:     require('./layouts'),
-  markdown:    require('./markdown'),
-  permalinks:  require('./permalinks'),
-  sass:        require('./sass'),
-  uglify:      require('./uglify'),
-  watch:       require('./watch')
+  assets:      require("./assets"),
+  collections: require("./collections"),
+  copy:        require("./copy"),
+  dateFormat:  require("./dateFormat"),
+  defaults:    require("./defaults"),
+  helpers:     require("./helpers"),
+  layouts:     require("./layouts"),
+  markdown:    require("./markdown"),
+  permalinks:  require("./permalinks"),
+  sass:        require("./sass"),
+  uglify:      require("./uglify"),
+  watch:       require("./watch")
 };
 
 //=======================================
@@ -40,22 +40,22 @@ var config = {
 //=======================================
 
 metalsmith(__dirname)
-  .clean(false)
-  .source('../' + config.defaults.src)
-  .destination('../' + config.defaults.dest)
-  .metadata(config.defaults,            log.step('Defaults Defined'))
-  .use(markdown(config.markdown),       log.step('Markdown Files to HTML'))
-  .use(drafts(),                        log.step('Drafts Hidden'))
-  .use(dateFormat(config.dateFormat),   log.step('Dates Formatted'))
-  .use(collections(config.collections), log.step('Collections Defined'))
-  .use(permalinks(config.permalinks),   log.step('Permalinks Created'))
-  .use(layouts(config.layouts),         log.step('Layouts Applied'))
-  .use(inplace(config.layouts),         log.step('Templating Rendered'))
-  .use(datamarkdown(config.markdown),   log.step('Markdown Content to HTML'))
-  .use(assets(config.assets),           log.step('Assets Folder Copied'))
-  .use(copy(config.copy),               log.step('Repo Folder Copied'))
-  .use(prefix(),                        log.step('CSS Prefixed'))
-  .use(sass(config.sass),               log.step('SCSS Processed'))
-  .use(uglify(config.uglify),           log.step('JS Uglified'))
+  .clean(true)
+  .source("../" + config.defaults.src)
+  .destination("../" + config.defaults.dest)
+  .metadata(config.defaults,            log.step("Defaults Defined"))
+  .use(markdown(config.markdown),       log.step("Markdown Files to HTML"))
+  .use(drafts(),                        log.step("Drafts Hidden"))
+  .use(dateFormat(config.dateFormat),   log.step("Dates Formatted"))
+  .use(collections(config.collections), log.step("Collections Defined"))
+  .use(permalinks(config.permalinks),   log.step("Permalinks Created"))
+  .use(layouts(config.layouts),         log.step("Layouts Applied"))
+  .use(inplace(config.layouts),         log.step("Templating Rendered"))
+  .use(datamarkdown(config.markdown),   log.step("Markdown Content to HTML"))
+  .use(assets(config.assets),           log.step("Assets Folder Copied"))
+  .use(copy(config.copy),               log.step("Repo Folder Copied"))
+  .use(prefix(),                        log.step("CSS Prefixed"))
+  .use(sass(config.sass),               log.step("SCSS Processed"))
+  .use(uglify(config.uglify),           log.step("JS Uglified"))
   .use(watch(config.watch))
   .build(log.build);
